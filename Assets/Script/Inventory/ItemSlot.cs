194fb31972bce45ca9fcebcfce1c1e178a5936ce
nano1412_PC <54862667+nano1412@users.noreply.github.com>
2026-01-12 00:11:45 +0700

    CP
    OnComsumableSelectionChange being ass

--- Assets/Script/Inventory/ItemSlot.cs
+++ Assets/Script/Inventory/ItemSlot.cs
@@ -1,24 +1,58 @@
+using NUnit.Framework.Interfaces;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using static UnityEditor.Timeline.Actions.MenuPriority;
 using static Utils;
 
-public class ItemSlot : MonoBehaviour, IPointerClickHandler
+public class ItemSlot : MonoBehaviour
 {
     [field: SerializeField] public ItemType AcceptableItemType { get; private set; }
     [field: SerializeField] public SlotType TypeOfSlot { get; private set; }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        
-        if (!(TypeOfSlot == SlotType.Shop || TypeOfSlot == SlotType.Display) && transform.GetComponentInChildren<ItemData>() != null)
-        {
-            EquipmentInventory.current.SetInventorySelectItem(transform.GetComponentInChildren<ItemData>().gameObject);
+    [field: SerializeField] public GameObject MiniItemDetail { get; private set; }
+    [field: SerializeField] public TMP_Text ItemName { get; private set; }
+    [field: SerializeField] public TMP_Text ItemShortDescription { get; private set; }
 
-            if(transform.GetComponentInChildren<ConsumableData>() != null)
-            {
-                BuffController.current.SelectedConsumable = transform.GetComponentInChildren<ConsumableData>().gameObject;
-            }
+    private void Start()
+    {
+        BuffController.current.OnSelectedConsumableUpdate += OnComsumableSelectionChange;
+    }
+
+    private void OnDestroy()
+    {
+        BuffController.current.OnSelectedConsumableUpdate -= OnComsumableSelectionChange;
+    }
+
+
+    void OnComsumableSelectionChange(GameObject itemGO)
+    {
+        Debug.Log("1");
+        if(itemGO == null)
+        {
+            Debug.Log("2");
+            MiniItemDetail.SetActive(false);
+            Debug.Log("3");
+            transform.GetComponentInChildren<ItemData>().gameObject.GetComponent<Outline>().enabled = false;
+            Debug.Log("4");
+            return;
+        }
+        
+        if (transform.GetComponentInChildren<ItemData>() == itemGO.GetComponent<ItemData>())
+        {
+            
+            MiniItemDetail.SetActive(true);
+            transform.GetComponentInChildren<ItemData>().gameObject.GetComponent<Outline>().enabled = true;
+        }
+    }
+
+    private void OnTransformChildrenChanged()
+    {
+        if(transform.GetComponentInChildren<ItemData>() != null)
+        {
+            ItemName.text = transform.GetComponentInChildren<ItemData>().ItemName;
+            ItemShortDescription.text = transform.GetComponentInChildren<ItemData>().ShortDescription;
         }
     }
 }
